e3c50ae43441cd1f264d2b8348f22f2adb4f8a4a
CLARA LIMA DE ALMEIDA <20232014040034@ifrn.local>
2024-03-21 15:06:36 -0300

    lista03

---
+++ Lista04/Q03.py
@@ -0,0 +1,42 @@
+class Playlist:
+    def __init__(self, nome, descricao):
+        self.__nome = nome
+        self.__descricao = descricao
+        self.__musicas = []
+    def set_nome(self, n):
+        self.__nome = n
+
+    def set_descricao(self, d):
+        self.__descricao = d
+
+    def set_musicas(self):
+        pass
+
+    def get_nome(self):
+        return self.__nome
+
+    def get_descrição(self):
+        return self.__descricao
+
+    def get_musicas(self):
+        return self.__musicas
+
+    def inserir(self, m):
+        self.__musicas.append(m)
+    
+    def listar(self):
+        return self.__musicas
+    
+    def __str__(self):
+        return f'Nome da música: {self.__musicas}\nDescrição: {self.__descricao}'
+class UI:
+
+    def main():
+        n = input()
+        d = input()
+        p = Playlist(n, d)
+
+        print(p)
+
+
+UI.main()
