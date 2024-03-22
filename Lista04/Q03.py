f361654cb1da6f7c93f199aac3e095c22d3d3edb
CLARA LIMA DE ALMEIDA <20232014040034@ifrn.local>
2024-03-22 12:01:39 -0300

    atualizacao

--- Lista04/Q03.py
+++ Lista04/Q03.py
@@ -1,4 +1,5 @@
 class Playlist:
+
     def __init__(self, nome, descricao):
         self.__nome = nome
         self.__descricao = descricao
@@ -16,7 +17,6 @@ class Playlist:
     def get_descrição(self):
         return self.__descricao
 
-
     def inserir(self, m):
         self.__musicas.append(m)
     
@@ -26,23 +26,76 @@ class Playlist:
     def __str__(self):
         return f'Total de Músicas na Playlist: {len(self.__musicas)}'
 
+
 class Musica:
+
     def __init__(self, titulo, artista, album):
         self.__titulo = titulo
         self.__artista = artista
         self.__album = album
+    
+    def set_titulo(self, novo_titulo):
+        self.__titulo = novo_titulo
+    
+    def set_artista(self, novo_artista):
+        self.__artista = novo_artista
+    
+    def set_album(self, novo_album):
+        self.__album = novo_album
+    
+    def get_titulo(self):
+        return self.__titulo
+    
+    def get_artista(self):
+        return self.__artista
+
+    def get_album(self):
+        self.__album
 
     def __str__(self):
         return f'Título da Música: {self.__titulo} Artista: {self.__artista} Album: {self.__album}'
 
+
 class UI:
 
     def main():
-        n = input("Nome da Música: ")
-        d = input("Descrição: ")
-        p = Playlist(n, d)
-
-        print(p)
-
+      play = None
+      op = 0
+      while op != 9:
+          op = UI.menu()
+          if op == 1: 
+              play = UI.nova_playlist()
+          if op == 2: 
+              if play == None: 
+                  print("Crie uma nova playlist antes!")
+              else:
+                  musica = UI.nova_musica()
+                  play.inserir(musica)
+          if op == 3: 
+              if play == None: 
+                  print("Crie uma nova playlist antes!")
+              else:
+                  for musica in play.listar():
+                      print(musica)     
+    
+    def nova_playlist():
+      nome = input("Informe o nome da playlist: ")
+      desc = input("Informe uma descrição: ")
+      playlist = Playlist(nome, desc)
+      return playlist
+    
+    def nova_musica():
+      titulo = input("Informe o título da música: ")
+      artista = input("Informe o artista ou banda: ")
+      album = input("Informe o álbum: ")
+      musica = Musica(titulo, artista, album)
+      return musica
+    
+    def menu():
+        print("1 - Nova PlayList")
+        print("2 - Inserir música")
+        print("3 - Listar músicas")
+        print("9 - fim")
+        return int(input("Opção: "))
 
 UI.main()
