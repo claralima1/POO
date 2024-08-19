4aabf31cf22db49b18faad9c9ae7698dd751e679
Clara Lima <86741485+claralima1@users.noreply.github.com>
2024-08-19 17:23:16 +0000

    atualizacao

--- Projeto/cliente.py
+++ Projeto/cliente.py
@@ -1,5 +1,4 @@
-import json
-import datetime
+import json, datetime
 # Modelo - POJO POCO
 class Cliente:
     def __init__(self, id, nome, email, fone):
@@ -7,98 +6,35 @@ class Cliente:
         self.nome = nome
         self.email = email
         self.fone = fone
-    def __str__(self):
-        return f"{self.id} - {self.nome} - {self.email} - {self.fone}"
     
     def set_nome(self, n):
         self.nome = n
     
+    def get_nome(self):
+        return self.nome
+
     def set_email(self, e):
         self.email = e
     
-    def set_nome(self, f):
-        self.fone = f
-    
-    def get_nome(self):
-        return self.nome
-    
     def get_email(self):
         return self.email
-    
+
+    def set_fone(self, f):
+        self.fone= f
+
     def get_fone(self):
-        return self.fone 
-    
-    def set_id(self, id):
-        self.id = id
-    
-    def get_idCliente(self):
-        return self.id
-    
-class Venda:
-    def __init__(self, id):
-        self.id = id
-        self.data = datetime.date
-        if self.total != 0:
-            self.carrinho = True
-        else: self.carrinho = False
-        self.total = VendaItem.qtd * VendaItem.preco
-        self.idCliente = Cliente.id
-
-    def set_idVenda(self, id):
-        self.id = id
-    
-    def get_idVenda(self):
-        return self.id
-    
-    def set_data(self, d):
-        self.data = d
-    
-    def get_data(self):
-        return self.data
-    
-    def get_carrinho(self):
-        return self.carrinho
-    
-    def get_nome(self):
-        return self.total
-
-    def __str__(self):
-        return f"{self.id}"
-    
-class VendaItem:
-    def __init__(self, id, qtd, preco):
-        self.id = id
-        self.qtd = qtd
-        self.preco = preco
-        self.idVenda = Venda.id
-        self.idProduto = Produto.id
-    
-    def set_qtd(self, q):
-        self.qtd = q
-    
-    def get_nome(self):
-        return self.qtd
-
-    def set_preco(self, p):
-        self.preco = p
-    
-    def get_nome(self):
-        return self.preco
+        return self.fone
 
     def __str__(self):
         return f"{self.id} - {self.nome} - {self.email} - {self.fone}"
 
 class Produto:
-    def __init__(self, id, d, p, e):
+    def __init__(self, id, desc, preco, estoque):
         self.id = id
-        self.desc = d
-        self.preco = p
-        self.estoque = e
-        self.idCategoria = Categoria.id
-
-    def __str__(self):
-        return f"{self.id} - {self.desc} - {self.preco} - {self.estoque}"
-
+        self.desc = desc
+        self.preco = preco
+        self.estoque = estoque
+    
     def set_desc(self, d):
         self.desc = d
     
@@ -107,23 +43,24 @@ class Produto:
     
     def set_preco(self, p):
         self.preco = p
-
+    
     def get_preco(self):
         return self.preco
     
-    def get_nome(self):
-        return self.preco
-
     def set_estoque(self, e):
         self.estoque = e
     
-    def get_nome(self):
-        return self.nome
+    def get_estoque(self):
+        return self.estoque
+
+    def __str__(self):
+        return f"{self.id} - {self.desc} - {self.preco} - {self.estoque}"
 
 class Categoria:
     def __init__(self, id, desc):
         self.id = id
         self.desc = desc
+
     def  __str__(self):
         return f"{self.id} - {self.desc}"
     
@@ -132,11 +69,37 @@ class Categoria:
     
     def get_desc(self):
         return self.desc
+    
+class VendaItem:
+    def __init__(self, id, qtd, p):
+        self.id = id
+        self.qtd = qtd
+        self.preco = p
+    
+    def set_qtd(self, q):
+        self.qtd = q
+    
+    def get_qtd(self):
+        return self.qtd
+    
+    def set_preco(self, p):
+        self.preco = p
+    
+    def get_preco(self):
+        return self.preco
+    
+class Venda:
+    def __init__(self, id):
+        self.id = id
+        #self.data = datetime.date()
+        #self.carrinho = True
+        #self.total
+
 
 # Persistência: Modelo -> Arquivo Json    
 class Clientes:
-    objetos = []
-                    # atributo da classe e não de uma instância da classe
+    objetos = []                # atributo da classe e não de uma instância da classe
+    
     @classmethod
     def inserir(cls, obj):      # create - C
         cls.abrir()             # abre a lista de clientes do arquivo
@@ -158,8 +121,8 @@ class Clientes:
         cls.abrir() 
         for x in cls.objetos:   # percorre a lista procurando o objeto com o id informado
             if x.id == id: return x
-        return None      
-    
+        return None   
+       
     @classmethod
     def atualizar(cls, obj):
         x = cls.listar_id(obj.id) # x é o objeto que já está na lista com o mesmo id do objeto novo
@@ -168,19 +131,19 @@ class Clientes:
             x.email = obj.email
             x.fone = obj.fone
             cls.salvar()
-    
+
     @classmethod
     def excluir(cls, obj):
         x = cls.listar_id(obj.id) # x é o objeto que já está na lista com o mesmo id do objeto novo
         if x != None: 
             cls.objetos.remove(x)
             cls.salvar()
-    
+
     @classmethod
     def salvar(cls):    
         with open("clientes.json", mode="w") as arquivo:
             json.dump(cls.objetos, arquivo, default = vars)
-    
+
     @classmethod
     def abrir(cls):
         cls.objetos = []
@@ -189,115 +152,6 @@ class Clientes:
             for obj in texto_arquivo:
                 c = Cliente(obj["id"], obj["nome"], obj["email"], obj["fone"])
                 cls.objetos.append(c)       
-class Vendas:
-    vendas = []
-
-    @classmethod
-    def inserir(cls, vends):
-        cls.abrir()
-        id = 0
-        for x in cls.vendas:
-            if x.id > id: id = x.id
-        id += 1
-        vends.id = id
-        cls.vendas.append(vends)
-        cls.salvar()
-
-    @classmethod
-    def listar(cls):
-        cls.abrir()
-        return cls.vendas
-    
-    @classmethod
-    def listar_id(cls, id):
-        cls.abrir()
-        for x in cls.vendas:
-            if x.id == id: return x
-        return None
-    
-    @classmethod
-    def atualizar(cls, v):
-        x = cls.listar_id(v.id)
-        if x!= None:
-            x.id = v.id
-            cls.salvar()
-
-    @classmethod
-    def excluir(cls, v):
-        x = cls.listar_id(v.id)
-        if x != None:
-            cls.vendas.remove(x)
-            cls.salvar()
-
-    @classmethod
-    def salvar(cls):
-        with open("vendas.json", mode="w") as arquivo:
-            json.dump(cls.vendas, arquivo, defaut = vars)
-    
-    @classmethod
-    def abrir(cls):
-        cls.vendas = []
-        with open("vendas.json", mode ="r") as arquivo:
-            texto_arquivo = json.load(arquivo)
-            for vend in texto_arquivo:
-                v = Vendas(vend["id"])
-                cls.vendas.append(v)
-
-class VendasItens:
-    vendas_itens = []
-    
-    @classmethod
-    def inserir(cls, item):
-        cls.abrir()
-        id = 0
-        for x in cls.vendas_itens:
-            if x.id > id: id = x.id
-        id +=1
-        item.id = id
-        cls.vendas_itens.append(item)
-        cls.salvar()
-
-    @classmethod
-    def listar(cls):
-        cls.abrir()
-        return cls.vendas_itens
-
-    @classmethod
-    def listar_id(cls, id):
-        cls.abrir()
-        for x in cls.vendas_itens:
-            if x.id == id: return x
-        return None
-    
-    @classmethod
-    def atualizar(cls, vend):
-        x = cls.listar_id(vend.id)
-        if x != None:
-            x.qtd = vend.qtd
-            x.preco = vend.preco
-            cls.salvar()
-
-    @classmethod
-    def excluir(cls, vend):
-        x = cls.listar_id(vend.id)
-        if x != None:
-            cls.vendas_itens.remove(x)
-            cls.salvar()
-
-    @classmethod
-    def salvar(cls):
-        with open("vendas_itens.json", mode="w") as arquivo:
-            json.dump(cls.vendas_itens, arquivo, default = vars)
-    
-    @classmethod
-    def abrir(cls):
-        cls.vendas_itens = []
-        with open("vendas_itens.json", mode="r") as arquivo:
-            texto_arquivo = json.load(arquivo)
-            for vend in texto_arquivo:
-                v = VendaItem(vend["id"], vend["qtd"], vend["preco"])
-                cls.vendas_itens.append(v)
-
 class Produtos:
     produtos = []
 
@@ -307,7 +161,7 @@ class Produtos:
         id = 0
         for x in cls.produtos:
             if x.id > id: id = x.id
-        d +=1
+        id += 1
         produto.id = id
         cls.produtos.append(produto)
         cls.salvar()
@@ -315,9 +169,7 @@ class Produtos:
     @classmethod
     def listar(cls):
         cls.abrir()
-        for x in cls.produtos:
-            if x.id == id: return x
-        return None
+        return cls.produtos
     
     @classmethod
     def listar_id(cls, id):
@@ -339,7 +191,7 @@ class Produtos:
     def excluir(cls, prod):
         x = cls.listar_id(prod.id)
         if x != None:
-            cls.produtos.remove(prod)
+            cls.produtos.remove(x)
             cls.salvar()
 
     @classmethod
@@ -353,9 +205,8 @@ class Produtos:
         with open("produtos.json", mode="r") as arquivo:
             texto_arquivo = json.load(arquivo)
             for  prod in texto_arquivo:
-                p = Produto(prod["id"], prod["descricção"], prod["preço"], prod["estoque"])
+                p = Produto(prod["id"], prod["desc"], prod["preco"], prod["estoque"])
                 cls.produtos.append(p)
-
 class Categorias:
     categorias = []
 
@@ -388,57 +239,74 @@ class Categorias:
         if x != None:
             x.desc = c.desc
             cls.salvar()
-
+        
+    @classmethod
     def excluir(cls, c):
-        x= cls.listar_id(c.id)
+        x = cls.listar_id(c.id)
         if x != None:
-            cls.categorias.remove(c)
+            cls.categorias.remove(x)
             cls.salvar()
 
     @classmethod
     def salvar(cls):
         with open("categorias.json", mode="w") as arquivo:
+            json.dump(cls.categorias, arquivo, default=vars)
+    
+    @classmethod
+    def abrir(cls):
+        cls.categorias = []
+        with open("categorias.json", mode="r") as arquivo:
             texto_arquivo = json.load(arquivo)
             for c in texto_arquivo:
-                c = Categorias(c["id"], c["descricão"])
-                cls.categorias.append(c)
+                cat = Categoria(c["id"], c["desc"])
+                cls.categorias.append(cat)
+
+class Vendas:
+    vendas = []
+
+    @classmethod
+    def inserir(cls, v):
+        cls.abrir()
+        id = 0
+        for x in cls.categorias:
+            if x.id> id: id = x.id
+        id += 1
+        v.id = id
+        cls.categorias.append(v)
+        cls.salvar()
 
 class UI:
     @staticmethod
     def menu():
-        print("Clientes")
-        print(" 1-Inserir Cliente, 1.1-Listar, 1.2-Atualizar, 1.3-Excluir, 9-Fim")
-        print("Produtos")
-        print(" 2 -Inserir Produto, 2.1-Listar Produto, 2.2-Atualizar Produto, 2.3-Excluir Produto, 9-Fim")
-        print("Categorias")
-        print(" 3 -Inserir categoria, 3.1-Listar categoria, 3.2-Atualizar categoria, 3.3-Excluir categoria, 9-Fim")
-        
+        print("CLIENTES")
+        print("  1-Inserir, 2-Listar, 3-Atualizar, 4-Excluir, 20-Fim")
+        print("\nPRODUTOS")
+        print("  5-Inserir, 6-Listar, 7-Atualizar, 8-Excluir, 20-Fim")
+        print("\nCATEGORIAS")
+        print("  9-Inserir, 10-Listar, 11-Atualizar, 12-Excluir, 20-Fim")
         return int(input("Escolha uma opção: "))
-        
-    
     
     @staticmethod
     def main():
         op = 0
-        while op != 9:
+        while op != 20:
             op = UI.menu()
-            #CLIENTES
             if op == 1: UI.cliente_inserir()
             if op == 2: UI.cliente_listar()
             if op == 3: UI.cliente_atualizar()
             if op == 4: UI.cliente_excluir()
             #PRODUTOS
-            if op == 2: UI.inserir_produtos()
-            if op == 2.1: UI.listar_produtos()
-            if op == 2.2: UI.atualizar_produtos()
-            if op == 2.3: UI.excluir_produto()
-            #CATEGORIA  
-            if op == 3: UI.inserir_categoria()
-            if op == 3.1: UI.listar_categoria()
-            if op == 3.2: UI.atualizar_categoria()
-            if op == 3.3: UI.excluir_categoria()
+            if op == 5: UI.inserir_produtos()
+            if op == 6: UI.listar_produtos()
+            if op == 7: UI.atualizar_produtos()
+            if op == 8: UI.excluir_produto()
+            #CATEGORIA
+            if op == 9: UI.inserir_categoria()
+            if op == 10: UI.listar_categoria()
+            if op == 11: UI.atualizar_categoria()
+            if op == 12: UI.excluir_categoria()
+            
 
-    #CLIENTES
     @staticmethod
     def cliente_inserir():
         #id = int(input("Informe o id: "))
@@ -470,15 +338,15 @@ class UI:
         a = Cliente(id, "", "", "")
         Clientes.excluir(a)
 
-    #PRODUTOS       
+        #PRODUTOS       
     @staticmethod
     def inserir_produtos():
         desc = input("Informe qual o produto: ")
-        valor = int(input("Informe o valor do produto: "))
+        valor = float(input("Informe o valor do produto: "))
         estoque = int(input("Informe a quantidade no estoque: "))
         p = Produto(0, desc, valor, estoque)
         Produtos.inserir(p)
-
+    
     @staticmethod
     def listar_produtos():
         for produtos in Produtos.listar():
@@ -489,7 +357,7 @@ class UI:
         UI.listar_produtos()
         id = int(input("Informe o id do produto a ser atualizado: "))
         desc = input("Informe qual o novo produto: ")
-        valor = int(input("Informe o valor do novo produto: "))
+        valor = float(input("Informe o valor do novo produto: "))
         estoque = int(input("Informe a quantidade atualizada do estoque: "))
         p = Produto(id, desc, valor, estoque)
         Produtos.atualizar(p)
@@ -500,8 +368,8 @@ class UI:
         id = int(input("Informe o id do produto a ser excluído: "))
         p = Produto(id, "","","")
         Produtos.atualizar(p)
-
-    #CATEGORIA
+    
+     #CATEGORIA
     @staticmethod
     def inserir_categoria():
         categoria = input("Informe a categoria do produto: ")
@@ -524,9 +392,9 @@ class UI:
     @staticmethod
     def excluir_categoria():
         UI.listar_categoria()
-        id = int(input("Informe a categoria a ser excluída: "))
+        id = int(input("Informe o id da categoria a ser excluída: "))
         c = Categoria(id, "")
         Categorias.excluir(c)
-
+    
 
 UI.main()
